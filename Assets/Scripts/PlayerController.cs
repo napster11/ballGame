9592c092528a10c4f4a2274702e82b8179b27ccf
Shivam <shsingh@tekion.com>
2018-10-01 15:51:20 -0500

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private int penalty;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        penalty = 10;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        if (other.gameObject.CompareTag("Obstacle")) {
+            penalty = penalty + 5;
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 5)
+        {
+            if (penalty >= 50)
+            {
+                winText.text = "You lose";
+            }
+            else
+            {
+                winText.text = "You Win!";
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/movingTarget.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class movingTarget : MonoBehaviour {
+
+    public float timer;
+    public int newtarget;
+    public float speed;
+    public NavMeshAgent nav;
+    public Vector3 Target;
+    private GameObject[] RandomPoint;
+    private int current;
+
+
+    void Start() {
+        nav = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        if(timer >= newtarget) {
+            newTarget();
+            timer = 0;
+            nav.speed = speed;
+        }
+    }
+
+    void newTarget() {
+        float newX = gameObject.transform.position.x;
+        float newZ = gameObject.transform.position.z;
+        float xPos = newX + Random.Range(newX-100,newX+100);
+        float zPos = newZ + Random.Range(newZ-100,newZ+100);
+
+        Target = new Vector3(xPos, gameObject.transform.position.y, zPos);
+        nav.SetDestination(Target);
+    }
+
+    // Use this for initialization
+    //private void Start () {
+    //       nm = this.GetComponent<NavMeshAgent>();
+    //       RandomPoint = GameObject.FindGameObjectsWithTag("capsule");
+    //}
+
+    //// Update is called once per frame
+    //private void Update () {
+    //    if(nm.hasPath == false) {
+    //        current = Random.Range(0, RandomPoint.Length+1);
+    //        nm.SetDestination(RandomPoint[current].transform.position);
+    //    }
+    //}
+
+}
